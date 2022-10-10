5265260940595f976049421110e568eec5d905df
SendiMendi <115234606+SendiMendi@users.noreply.github.com>
2022-10-10 09:06:24 +0200

    Update streamlit_app.py
    New items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('Kale, SPinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+stramlit.header('Build your own fruit smoothie! 🍒🍓🍌')
